bf33c789e1b39f4049f5770159b5d8e120cad7a3
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2020-12-03 19:08:30 +0000

    basic page, with css and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home() {
-    return <div>Home</div>
-}
+import Link from 'next/link'
+import Image from 'next/image'
 
-export default Home
+export default function IndexPage() {
+  return (
+    <div>
+       
+      <h1 className="title">Hello World</h1><br/>
+      <Link href="/about">
+        <a>Sobre Mim</a>
+      </Link>
+      {/*<Image
+        alt="Next.js logo"
+        src="https://assets.vercel.com/image/upload/v1538361091/repositories/next-js/next-js-bg.png"
+        width={1200}
+        height={400}
+      />*/}
+    </div>
+  )
+}
